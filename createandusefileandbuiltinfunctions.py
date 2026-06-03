99432720bb3d761f31d4039e10d0d3a9ccefbca0
JanaaniParthasarathy-sketch <amrithajanaani@gmail.com>
2026-06-03 21:11:14 +0530

    added createandusefileandbuiltinfunctions.py

---
+++ createandusefileandbuiltinfunctions.py
@@ -0,0 +1,21 @@
+#create and use file
+def add(a, b):
+    return a + b
+
+result = add(10, 20)
+print(result)
+
+#firstt built in
+numbers = [10, 20, 30, 40]
+
+print(len(numbers))
+print(max(numbers))
+print(min(numbers))
+print(sum(numbers))
+
+#second built in
+name = "janaani"
+
+print(name.upper())
+print(name.capitalize())
+
