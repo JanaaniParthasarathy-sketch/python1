b6502333a41a1c03c546a78024e39a2d49039f0c
JanaaniParthasarathy-sketch <amrithajanaani@gmail.com>
2026-06-03 20:55:37 +0530

    added lists.py

---
+++ lists.py
@@ -0,0 +1,19 @@
+# Creating Lists
+fruits = ["Apple", "Banana", "Mango"]
+print(fruits)
+
+# Accessing Elements
+print(fruits[0])
+print(fruits[2])
+
+# Adding Elements
+fruits.append("Orange")
+print(fruits)
+
+# Removing Elements
+fruits.remove("Banana")
+print(fruits)
+
+# Loop Through List
+for item in fruits:
+    print(item)
