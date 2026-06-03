d22edb1f88a017f99b24b81f169a39a1d498a581
JanaaniParthasarathy-sketch <amrithajanaani@gmail.com>
2026-06-03 21:02:12 +0530

    added tuples.py

---
+++ tuples.py
@@ -0,0 +1,14 @@
+# Creating a tuple
+fruits = ("apple", "banana", "mango")
+
+print(fruits)
+print(fruits[1])  
+
+# Tuple unpacking
+person = ("Janaani", 18, "Chennai")
+
+name, age, city = person
+
+print(name)
+print(age)
+print(city)
