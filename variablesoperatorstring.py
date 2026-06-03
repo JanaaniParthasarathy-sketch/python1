a2519e3b8bff6a95bc4bff6821adc0c8a4960763
JanaaniParthasarathy-sketch <amrithajanaani@gmail.com>
2026-06-03 20:51:54 +0530

    added  variablesoperatorstring.py

---
+++ variablesoperatorstring.py
@@ -0,0 +1,68 @@
+#variables
+name = "Janaani"
+age = 18
+cgpa = 8.9
+
+print(name)
+print(age)
+print(cgpa)
+
+#operators
+a = 15
+b = 4
+
+print(a + b)   
+print(a - b)   
+print(a * b)   
+print(a / b)   
+print(a // b)  
+print(a % b)   
+print(a ** b)  
+
+#comparison operator
+x = 10
+y = 20
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+
+#logical operator
+age = 20
+citizen = True
+
+print(age >= 18 and citizen)
+print(age < 18 or citizen)
+print(not citizen)
+
+#creating string
+
+name = "Janaani"
+
+print(name)
+print(len(name))
+
+#string slicing
+
+text = "Python"
+
+print(text[0])
+print(text[1:4])
+print(text[-1])
+
+#string methods
+msg = "hello world"
+
+print(msg.upper())
+print(msg.lower())
+print(msg.capitalize())
+print(msg.replace("world", "python"))
+
+#string concateation
+first = "Artificial"
+second = "Intelligence"
+
+print(first + " " + second)
+
+
