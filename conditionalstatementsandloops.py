1504eb0916024116378fb599afc371bc179ef297
JanaaniParthasarathy-sketch <amrithajanaani@gmail.com>
2026-06-03 21:06:07 +0530

    added conditionalstatementsandloops.py

---
+++ conditionalstatementsandloops.py
@@ -0,0 +1,32 @@
+#conditional statements
+age = int(input("Enter age: "))
+
+if age >= 18:
+    print("Eligible to vote")
+else:
+    print("Not eligible to vote")
+
+#second
+marks = 85
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 75:
+    print("Grade B")
+else:
+    print("Grade C")
+
+#for loops 
+fruits = ["Apple", "Banana", "Mango"]
+
+for fruit in fruits:
+    print(fruit)
+
+#while loop
+count = 1
+
+while count <= 5:
+    print(count)
+    count += 1
+
+#
